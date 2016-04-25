6cd151335021e22c6ca6233d4a8fc17b22f320cf
jinh13 <jin.hui@wustl.edu>
2016-04-25 18:23:35 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,25 @@
-// loop
-var count = 0;
-var list = '';
+var count = 1;
+var list = "";
 
-while (count < 10) {
-  list += '<li>current count is: ' + count + '</li>';
-  count += 1;
+while (count < 11){
+	list += '<li> current count is: ' + count + '</li>';
+	count += 1;
 }
 
-// output results
 document.write("<h1>Loops</h1>");
-document.write("<ul>" + list + "</ul>");
+document.write("<ol>"+ list + "</ol>");
+
+var myItems = [
+"question one",
+"question two"
+];
+
+var secondList = "<ul>";
+var i =0;
+
+while (i < myItems.length)
+{
+	secondList += "<li>" + myItems[i] + "</li>";
+};
+
+document.write("<p>" + myItems[0] + "</p>");
